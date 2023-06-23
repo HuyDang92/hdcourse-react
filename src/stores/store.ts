0aa7fdb7c3924a34f1575d1c99eea129e0ef249e
Phuc262002 <81422862+Phuc262002@users.noreply.github.com>
2023-06-23 11:57:37 +0700

    hide react dev tool on production

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -9,6 +9,8 @@ export const store = configureStore({
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(homeApi.middleware),
+
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 setupListeners(store.dispatch);
